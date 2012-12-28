36e63ba7137894923ba500fe0e109f5395e81715
Doug Finke <finked@hotmail.com>
2012-12-28 10:20:52 -0500

    Reduce the # of lines

--- wget.ps1
+++ wget.ps1
@@ -1,22 +1,8 @@
-# Simple PowerShell script to get data from web
-# It's inspired on *nix wget
-# Author: Adler Medrado
-# http://adlermedrado.com.br
-# Under the MIT License
+param(
+    [Parameter(Mandatory)]
+    $url,
+    [Parameter(Mandatory)]
+    $name
+)
 
-$url=$args[0]
-$name=$args[1]
-$error="Like this: ./wget.ps1 http://mysite.com myfilename.txt"
-
-if ($url) {
-	if([string]::IsNullOrEmpty($name)) {            
-	    Write-Host "You must give a name to downloaded file"
-	    Write-Host $error
-	} else {
-		Write-host Wait a minute. Getting $url
-		Invoke-WebRequest $url -OutFile $name            
-	}
-} else {
-	Write-Host You must provide an URL
-	Write-Host $error	
-}
+Invoke-WebRequest $url -OutFile $name
